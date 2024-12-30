4e4d2ee30fae3953aa3eaae874d1f011529a1b47
gulumjan <zakirovagulumjan53@gmail.com>
2024-12-30 17:23:09 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
+import LayoutSite from "@/components/layout/LayoutSite";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <LayoutSite>{children}</LayoutSite>
       </body>
     </html>
   );
