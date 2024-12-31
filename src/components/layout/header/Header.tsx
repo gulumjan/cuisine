85adc5d32e403fabfffc60d6cc04c44860c6d812
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-12-31 09:47:33 +0600

    случайно запушил на main

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,7 +5,30 @@ const Header: FC = () => {
   return (
     <section className={scss.Header}>
       <div className="container">
-        <div className={scss.content}>Header</div>
+        <div className={scss.content}>
+          <div className={scss.logo}>
+            <h1>Restaurant</h1>
+          </div>
+
+          <div className={scss.nav}>
+            <ul>
+              <li>Interior</li>
+              <li>About Us</li>
+              <li>Menu</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <div className={scss.active_block}>
+            <div className={scss.search}>
+              <input type="text" placeholder="Search"/>
+            </div>
+            <div className={scss.lng_switch}>
+              <p>EN</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
   );
