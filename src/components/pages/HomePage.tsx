cc2e9a89ffa90acbdeebd44b91dc20a57c2e7cf7
gulumjan <zakirovagulumjan53@gmail.com>
2025-01-01 14:33:39 +0600

    Main menu

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import MainMenu from "./home_pages/gulumjan/main_menu/MainMenu";
 
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <>
+      HomePage
+      <MainMenu />
+    </>
+  );
 };
 
 export default HomePage;
---
+++ src/datas/categories.ts
@@ -0,0 +1,184 @@
+// data/categories.ts
+export const categories = [
+  { id: 1, name: "Desserts" },
+  { id: 2, name: "Hot Drinks" },
+  { id: 3, name: "Cold Drinks" },
+  { id: 4, name: "National Food" },
+  { id: 5, name: "Eastern cuisine" },
+  { id: 6, name: "Fast Foods" },
+];
+
+export const menuItems = {
+  Desserts: [
+    {
+      name: "Cheesecake",
+      price: "$5.99",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Burger&Pasta",
+      price: "$14",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Burger&Pasta",
+      price: "$14",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Burger&Pasta",
+      price: "$14",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Burger&Pasta",
+      price: "$14",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+  ],
+  "Hot Drinks": [
+    {
+      name: "Latte",
+      price: "$3.50",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Green Tea",
+      price: "$2.50",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+  ],
+  "Cold Drinks": [
+    {
+      name: "Iced Coffee",
+      price: "$3.00",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Lemonade",
+      price: "$2.50",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+  ],
+  "Fast Foods": [
+    {
+      name: "Burger",
+      price: "$5.00",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+    {
+      name: "Fries",
+      price: "$2.50",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+  ],
+  "National Food": [
+    {
+      name: "Besh-Barmak",
+      price: "$10",
+      description:
+        "Lörem ipsum askstoppad defaktisk, logokemi. Diastat retos att endomatisk. Geogehet pultvätta, om pneumativ.",
+    },
+  ],
+};
+
+export const menus = {
+  Desserts: [
+    {
+      name: "Ice Cream",
+      price: "$9.11",
+      url: "/IceCream1.png",
+      composition: "soda,cream,mild,sugar",
+      extras: [
+        { name: "Cherry", price: "$0.90" },
+        { name: "Cherry", price: "$0.90" },
+      ],
+      drinks: [
+        { name: "Coca Cola", price: "$0.90" },
+        { name: "Coca Cola", price: "$0.90" },
+      ],
+    },
+    {
+      name: "Ice Cream",
+      price: "$9.11",
+      url: "/IceCream2.png",
+      composition: "soda,cream,mild,sugar",
+      extras: [
+        { name: "Cherry", price: "$0.90" },
+        { name: "Cherry", price: "$0.90" },
+      ],
+      drinks: [
+        { name: "Coca Cola", price: "$0.90" },
+        { name: "Coca Cola", price: "$0.90" },
+      ],
+    },
+    {
+      name: "Ice Cream",
+      price: "$9.11",
+      url: "/IceCream3.png",
+      composition: "soda,cream,mild,sugar",
+      extras: [
+        { name: "Cherry", price: "$0.90" },
+        { name: "Cherry", price: "$0.90" },
+      ],
+      drinks: [
+        { name: "Coca Cola", price: "$0.90" },
+        { name: "Coca Cola", price: "$0.90" },
+      ],
+    },
+    {
+      name: "Ice Cream",
+      price: "$9.11",
+      url: "/IceCream4.png",
+      composition: "soda,cream,mild,sugar",
+      extras: [
+        { name: "Cherry", price: "$0.90" },
+        { name: "Cherry", price: "$0.90" },
+      ],
+      drinks: [
+        { name: "Coca Cola", price: "$0.90" },
+        { name: "Coca Cola", price: "$0.90" },
+      ],
+    },
+    {
+      name: "Ice Cream",
+      price: "$9.11",
+      url: "/IceCream5.png",
+      composition: "soda,cream,mild,sugar",
+      extras: [
+        { name: "Cherry", price: "$0.90" },
+        { name: "Cherry", price: "$0.90" },
+      ],
+      drinks: [
+        { name: "Coca Cola", price: "$0.90" },
+        { name: "Coca Cola", price: "$0.90" },
+      ],
+    },
+    {
+      name: "Ice Cream",
+      price: "$9.11",
+      url: "/IceCream6.png",
+      composition: "soda,cream,mild,sugar",
+      extras: [
+        { name: "Cherry", price: "$0.90" },
+        { name: "Cherry", price: "$0.90" },
+      ],
+      drinks: [
+        { name: "Coca Cola", price: "$0.90" },
+        { name: "Coca Cola", price: "$0.90" },
+      ],
+    },
+  ],
+  "Hot Drinks": [],
+};
